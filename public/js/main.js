9648b763bb02f16c452ef69aee05389dcfd7ab11
Doortje <doortje.spanjerberg@maximum.nl>
2019-11-26 23:17:50 +0100

    accordion now work

--- public/js/main.js
+++ public/js/main.js
@@ -43,14 +43,44 @@
 //     });
 // }
 
+function getParent(element, selector) {
+    // Get the next parent element
+    let parent = element.parentElement;
+
+    // If the sibling matches our selector, use it
+    // If not, jump to the next sibling and continue the loop
+    while (parent) {
+        if (parent.matches(selector)) {
+            return parent;
+        }
+        parent = parent.parentElement
+    }
+}
+
 function initAccordion(htmlWrapper) {
     let allAccordions = htmlWrapper.getElementsByClassName('js-accordion');
+    let allAccordionToggles = htmlWrapper.getElementsByClassName('js-accordion-toggle');
 
     for (let i = 0; i < allAccordions.length; i++) {
         let heightOfElement = allAccordions[i].offsetHeight;
         allAccordions[i].style.height = heightOfElement + "px";
         allAccordions[i].style.maxHeight = heightOfElement + "px";
         allAccordions[i].classList.add('is-closed');
+        let parent = getParent(allAccordions[i], '.js-list-item');
+        parent.classList.add('is-closed');
+
+        allAccordionToggles[i].addEventListener('click', function() {
+            let parentOfClickedButton = getParent(this, '.js-list-item');
+
+            if(parentOfClickedButton.classList.contains('is-closed')) {
+                parentOfClickedButton.classList.remove('is-closed');
+                parentOfClickedButton.classList.add('is-expanded');
+            }
+            else {
+                parentOfClickedButton.classList.remove('is-expanded');
+                parentOfClickedButton.classList.add('is-closed');
+            }
+        })
     }
 }
 
